a7dd5936402457a45f6684faf752e82b2a0c77c8
Joe Francia <Jordan.Francia@viacom.com>
2016-03-17 17:18:47 -0400

    Added addAges and addPortraits

--- people.js
+++ people.js
@@ -1,13 +1,17 @@
 'use strict';
 var sunlightApiKey = 'c7da72ec53894f039491c87023661f8e';
+var openSecretsApiKey = '35776fd4c0bb1d6f8153182389162f86';
 var sunlightLegislatorLocateUrl = 'http://congress.api.sunlightfoundation.com/legislators/locate/';
+
 
 function People(zip) {
   this.query = {
     apikey: sunlightApiKey,
     zip: zip
   };
-  this.getPeople();
+  this.callSunlightApi();
+  this.addPortraits();
+  this.addAges();
 }
 
 People.prototype.saveResponse = function saveResponse(response) {
@@ -26,3 +30,21 @@ People.prototype.callSunlightApi = function callSunlightApi() {
   });
 };
 
+People.prototype.addPortraits = function addPortraits(){
+  for(person in this.people){
+    person.portrait = "https://www.govtrack.us/data/photos/" + person.govtrack_id + ".jpeg";
+  }
+}
+
+People.prototype.addAges = function addAges(){
+  var currentDate = new Date();
+  for(person in this.people){
+    debugger;
+    var bday = person.birthday.split('-');
+    var birthday = new Date(bday[0], bday[1], bday[2]);
+    var age = (currentDate - birthday).getFullYear();
+    person.age = age;
+  }
+}
+
+People.prototype.addIndustries = function addIndustries(){}
