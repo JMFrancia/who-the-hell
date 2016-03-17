91ecdb3db7bd6cae837a29cd6159ea6ff5551faf
yining <yining.chen@viacommix.com>
2016-03-17 15:18:49 -0400

    switching to basic web app

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
 'use strict';
-
-var express = require('express');
-var app = express();
-
-app.get('/', function (req, res) {
-  res.send('World!');
-});
-
-app.listen(3000, function () {
-  console.log('listening on port 3000!');
-});
